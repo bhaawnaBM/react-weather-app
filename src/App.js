f61ff723894a890e9a2e61c22e28e0456b6d4752
bhaawnaBM <bhaawna1805@gmail.com>
2023-04-27 13:32:15 +1000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Bhaawna M and is
-        <a
-          href="https://github.com/bhaawnaBM/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Bhaawna M and is
+          <a
+            href="https://github.com/bhaawnaBM/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
