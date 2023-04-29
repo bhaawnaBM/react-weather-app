15464e70f213e9110ecb4b2b0bd947b360cdfbf9
bhaawnaBM <bhaawna1805@gmail.com>
2023-04-29 19:48:12 +1000

    api integration

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setweatherData] = useState({ ready: false });
 
   function showTemperature(response) {
@@ -65,8 +65,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "094780c710fa4efd669f0df8c3991927";
-    let city = "London";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showTemperature);
   }
   return "Loading......";
