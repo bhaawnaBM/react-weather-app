b0abb2ef61e21d885daafd6ccc9f3809a0b5d9e2
bhaawnaBM <bhaawna1805@gmail.com>
2023-05-02 21:25:01 +1000

    Include the initial draft of forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherForecast from "./WeatherForecast";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,7 +17,7 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-6">
           <div className="d-flex">
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={50} />
             <div className="float-left">
               <WeatherTemperature celcius={props.data.temperature} />
             </div>
@@ -29,6 +30,7 @@ export default function WeatherInfo(props) {
           </ul>
         </div>
       </div>
+      <WeatherForecast />
     </div>
   );
 }
