20b789e896b6a5fd6a0cde00505b372b644949f9
bhaawnaBM <bhaawna1805@gmail.com>
2023-05-06 22:25:03 +1000

    removed forecastDay

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
-//import WeatherForecastDay from "./WeatherForecastDay";
+
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
